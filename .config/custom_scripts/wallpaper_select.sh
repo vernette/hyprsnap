c8512523ed29e3284003fe455f11d563ba6b364a
Skryabin Nikita <nekit2271@gmail.com>
2024-03-02 16:11:46 +0300

    Add custom scripts folder and change wallpaper selector style

---
+++ .config/custom_scripts/wallpaper_select.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+image_dir="$HOME/.wallpapers"
+images=("$image_dir"/*.png)
+
+image_list=""
+for img in "${images[@]}"; do
+    image_list+=$(basename "$img" | cut -d. -f1)"\x00icon\x1f${img}\n"
+done
+
+selected_image=$(printf '%b' "$image_list" | rofi -dmenu -theme ~/.config/rofi/wallpaper-select.rasi -p "Select wallpaper")
+
+for img in "${images[@]}"; do
+    if [[ "$(basename "$img" | cut -d. -f1)" = "$selected_image" ]]; then
+        selected_image_path="$img"
+        break
+    fi
+done
+
+if [ -n "$selected_image_path" ]; then
+  ln -sf "$selected_image_path" ~/Pictures/wallpaper.png
+  hyprctl reload
+  notify-send "Wallpaper changed" "$selected_image_path" -i ~/Pictures/wallpaper.png
+fi
+
