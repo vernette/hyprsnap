7982b5e14064b3232aba9ffd11612008b23f89b5
Skryabin Nikita <nekit2271@gmail.com>
2024-03-14 17:39:20 +0300

    Add lsp completions

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -33,7 +33,7 @@ return {
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
-					-- { name = 'nvim_lsp' },
+					{ name = 'nvim_lsp' },
 					{ name = "luasnip" },
 				}, {
 					{ name = "buffer" },
