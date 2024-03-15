461f7350f234013559cb3e558037bbebd025a2c3
Skryabin Nikita <nekit2271@gmail.com>
2024-03-15 21:34:51 +0300

    Add case insensitive search

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,4 +3,6 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set tabstop=2")
 vim.cmd("set noswapfile")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
 vim.g.mapleader = " "
