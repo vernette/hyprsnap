f57378c4389e1008ed41cd7443ecb43b06764227
Skryabin Nikita <nekit2271@gmail.com>
2024-03-16 11:42:45 +0300

    New plugins

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -5,10 +5,9 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>')
-    vim.keymap.set('n', '<leader>e', ':Neotree focus<CR>')
-  end
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>")
+    vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>")
+  end,
 }
