9f8a8981017a68399ba4c592e81eee934f69649a
Skryabin Nikita <nekit2271@gmail.com>
2024-03-16 22:37:46 +0300

    Add noice plugin

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+  config = function()
+    require("noice").setup()
+  end
+}
