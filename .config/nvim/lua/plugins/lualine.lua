b832e3044f445c5235dffcdfc71a9dee284295fb
Skryabin Nikita <nekit2271@gmail.com>
2024-05-13 15:26:17 +0300

    Add Codeium AI

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,31 +1,35 @@
+local function codeium()
+	return "󱃖 Codeium:" .. vim.fn["codeium#GetStatusString"]()
+end
+
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "pywal",
-        globalstatus = true,
-        section_separators = "",
-        component_separators = "",
-        disabled_filetypes = {
-          "alpha",
-        },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { "diff", "diagnostics" },
-        lualine_x = { "encoding", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      extensions = {
-        "mason",
-        "nvim-tree",
-        "nvim-dap-ui",
-        "toggleterm",
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "pywal",
+				globalstatus = true,
+				section_separators = "",
+				component_separators = "",
+				disabled_filetypes = {
+					"alpha",
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "diff", "diagnostics" },
+				lualine_x = { codeium, "encoding", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			extensions = {
+				"mason",
+				"nvim-tree",
+				"nvim-dap-ui",
+				"toggleterm",
+			},
+		})
+	end,
 }
