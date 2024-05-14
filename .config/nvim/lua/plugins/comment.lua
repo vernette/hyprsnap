70d08a28915add26cd5f8005fcf9d006faf8d135
Skryabin Nikita <nekit2271@gmail.com>
2024-05-14 15:56:06 +0300

    Change keymaps

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -2,12 +2,14 @@ return {
   "numToStr/Comment.nvim",
   lazy = false,
   config = function()
-    require("Comment").setup()
-
-    vim.keymap.set("n", "<Leader>/", function()
-      return vim.v.count == 0 and "<Plug>(comment_toggle_linewise_current)"
-          or "<Plug>(comment_toggle_linewise_count)"
-    end, { expr = true })
-    vim.keymap.set("x", "<Leader>/", "<Plug>(comment_toggle_linewise_visual)")
+    require("Comment").setup({
+      padding = true,
+      toggler = {
+        line = "<leader>/",
+      },
+      opleader = {
+        line = "<leader>/",
+      },
+    })
   end,
 }
