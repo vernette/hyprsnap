77ee53522d1b29abdbf46960c36a653d1cde0261
Skryabin Nikita <nekit2271@gmail.com>
2024-05-14 13:55:04 +0300

    New options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -11,4 +11,13 @@ vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.colorcolumn = "79"
 vim.opt.fillchars = {eob = " "}
+vim.opt.fileencoding = "utf-8"
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.undofile = true
+vim.opt.updatetime = 300
+vim.opt.numberwidth = 2
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
 vim.g.mapleader = " "
