a64883730511104950d23123dd63f492b9fee00a
Skryabin Nikita <nekit2271@gmail.com>
2024-05-14 15:56:20 +0300

    Add context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,39 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = {
-        "bash",
-        "html",
-        "json",
-        "jsonc",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "regex",
-        "yaml",
-        "hyprlang",
-        "rasi",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+		configs.setup({
+			ensure_installed = {
+				"bash",
+				"html",
+				"json",
+				"jsonc",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"regex",
+				"yaml",
+				"hyprlang",
+				"rasi",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			context_commentstring = {
+				enable = true,
+				enable_autocmd = false,
+			},
 
-      vim.filetype.add({
-        extension = { rasi = "rasi" },
-        pattern = {
-          [".*/waybar/config"] = "jsonc",
-          [".*/hypr/.*%.conf"] = "hyprlang",
-        },
-      }),
-    })
-  end,
+			vim.filetype.add({
+				extension = { rasi = "rasi" },
+				pattern = {
+					[".*/waybar/config"] = "jsonc",
+					[".*/hypr/.*%.conf"] = "hyprlang",
+				},
+			}),
+		})
+	end,
 }
