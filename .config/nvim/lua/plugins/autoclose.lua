051d567328e12322d1200d677138307924f161d1
Nikita Skryabin <nekit2271@gmail.com>
2024-06-06 12:31:47 +0300

    Enable all filetypes and pair_spaces

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -1,10 +1,12 @@
 return {
-	"m4xshen/autoclose.nvim",
-	config = function()
-		require("autoclose").setup({
-			options = {
-				disable_when_touch = true,
-			},
-		})
-	end,
+  "m4xshen/autoclose.nvim",
+  config = function()
+    require("autoclose").setup({
+      options = {
+        disabled_filetypes = {},
+        disable_when_touch = true,
+        pair_spaces = true
+      },
+    })
+  end,
 }
