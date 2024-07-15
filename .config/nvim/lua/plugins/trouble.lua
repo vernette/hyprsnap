45f9a49d5be807a11e530e9152e24278fec70df7
Nikita Skryabin <nekit2271@gmail.com>
2024-07-15 11:02:19 +0300

    feat: add trouble plugin

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/trouble.nvim",
+  cmd = "Trouble",
+  opts = {},
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+  }
+}
