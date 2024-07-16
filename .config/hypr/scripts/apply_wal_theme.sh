d51a4b6af071dbbc2097ae41f5de6b92d2a1b119
Nikita Skryabin <nekit2271@gmail.com>
2024-07-17 00:46:03 +0300

    refactor: rename constant

--- .config/hypr/scripts/apply_wal_theme.sh
+++ .config/hypr/scripts/apply_wal_theme.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-theme_file="/tmp/theme_variant"
+THEME_FILE="/tmp/theme_variant"
 wal_arguments=""
 
-if [ -s "$theme_file" ]; then
-  case $(<"$theme_file") in
+if [ -s "$THEME_FILE" ]; then
+  case $(<"$THEME_FILE") in
     "light") wal_arguments="lighten -l" ;;
   esac
 fi
 
-wal -i ~/Pictures/wallpaper.png --cols16 $wal_arguments -q -n -e
+wal -i ~/Pictures/wallpaper.png --cols16 "$wal_arguments" -q -n -e
 
 pgrep -x "waybar" > /dev/null && killall -SIGUSR2 waybar
 
