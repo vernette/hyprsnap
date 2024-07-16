ae9197be55b80f47dc5c0db8e5511ba1acb7b4d6
Nikita Skryabin <nekit2271@gmail.com>
2024-07-16 23:58:32 +0300

    refactor: add directory and image validation

--- .config/hypr/scripts/random_wallpaper.sh
+++ .config/hypr/scripts/random_wallpaper.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 image_dir="$HOME/Pictures/wallpapers"
-images=("$image_dir"/*.png)
+
+if [ ! -d "$image_dir" ]; then
+  notify-send -a "Wallpaper selector" "Images directory does not exist" "$image_dir"
+  exit 1
+fi
+
+images=("$image_dir"/*)
+
+if [ ${#images[@]} -eq 0 ]; then
+  notify-send -a "Wallpaper selector" "No images found in $image_dir"
+  exit 1
+fi
+
+current_image="$image_dir/wallpaper.png"
 
 random_image="${images[RANDOM % ${#images[@]}]}"
 
-if [ -z "$random_image" ]; then
-  echo "No images found in the directory."
-  exit 1
-fi
+while [ "$random_image" -ef "$current_image" ]; do
+	random_image="${images[RANDOM % ${#images[@]}]}"
+done
 
 ln -sf "$random_image" ~/Pictures/wallpaper.png
 
 if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
-  . ~/.config/hypr/scripts/set_wallpaper.sh
+	. ~/.config/hypr/scripts/set_wallpaper.sh
 else
-  i3-msg restart
+	i3-msg restart
 fi
 
 notify-send -a "Wallpaper selector" "Wallpaper changed" "$random_image" -i ~/Pictures/wallpaper.png
