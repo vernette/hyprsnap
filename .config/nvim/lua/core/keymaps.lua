52ced4b4eab617bb6e043d5c305c2082a236d84b
Nikita Skryabin <nekit2271@gmail.com>
2024-07-27 13:25:00 +0300

    feat: add translation plugin and keymaps for it

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -119,3 +119,6 @@ keymap_with_desc("t", "<esc>", [[<C-\><C-n>]], "Exit terminal mode")
 keymap_with_desc("n", "<leader>kn", function() require('kulala').jump_next() end, "Jump to the next request")
 keymap_with_desc("n", "<leader>kp", function() require('kulala').jump_prev() end, "Jump to the previous request")
 keymap_with_desc("n", "<leader>kr", function() require('kulala').run() end, "Send request")
+
+-- Pantran
+keymap_with_desc({"n", "x"}, "<leader>tr", ":Pantran<CR>", "Translate")
---
+++ .config/nvim/lua/plugins/misc/pantran.lua
@@ -0,0 +1,5 @@
+return {
+  "potamides/pantran.nvim",
+  cmd = "Pantran",
+  opts = { default_engine = "yandex" },
+}
