ff7a80cc354af9bd069af62b382763b10ee18545
Nikita Skryabin <nekit2271@gmail.com>
2024-07-28 00:04:21 +0300

    fix(gen.lua): update LLaMA model version from 3 to 3.1 for improved performance and accuracy

--- .config/nvim/lua/plugins/misc/gen.lua
+++ .config/nvim/lua/plugins/misc/gen.lua
@@ -2,7 +2,7 @@ return {
   "David-Kunz/gen.nvim",
   -- TODO: https://github.com/David-Kunz/gen.nvim?tab=readme-ov-file#usage
   opts = {
-    model = "llama3",
+    model = "llama3.1",
     display_mode = "split",
     show_prompt = true,
     show_model = true,
