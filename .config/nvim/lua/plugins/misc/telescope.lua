d774301041a0709c9940da7ad7be00919574f10e
Nikita Skryabin <nekit2271@gmail.com>
2024-07-27 15:21:01 +0300

    chore: change lazy load condition

--- .config/nvim/lua/plugins/misc/telescope.lua
+++ .config/nvim/lua/plugins/misc/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  cmd = "Telescope",
+  event = "VeryLazy",
   dependencies = {
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope-ui-select.nvim" },
