15278cbbcbcb00fe539b31e613b86a2f3b6295f4
Nikita Skryabin <nekit2271@gmail.com>
2024-07-27 15:01:11 +0300

    feat(gen.lua): add gen.nvim plugin configuration for Neovim

---
+++ .config/nvim/lua/plugins/misc/gen.lua
@@ -0,0 +1,11 @@
+return {
+  "David-Kunz/gen.nvim",
+  -- TODO: https://github.com/David-Kunz/gen.nvim?tab=readme-ov-file#usage
+  opts = {
+    model = "llama3",
+    display_mode = "split",
+    show_prompt = true,
+    show_model = true,
+    no_auto_close = true,
+  },
+}
