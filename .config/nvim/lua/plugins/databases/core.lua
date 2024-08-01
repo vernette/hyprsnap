e2ded1fb06132d199a401725cadf2f36095da33b
Nikita Skryabin <nekit2271@gmail.com>
2024-08-01 23:22:03 +0300

    feat: add database plugins

---
+++ .config/nvim/lua/plugins/databases/core.lua
@@ -0,0 +1,13 @@
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod" },
+    { "kristijanhusak/vim-dadbod-completion" },
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+}
