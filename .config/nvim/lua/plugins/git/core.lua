0665931229e2cc6d28303c942fff7dc424d8f3de
Nikita Skryabin <nekit2271@gmail.com>
2024-08-01 23:18:56 +0300

    refactor: formatting

--- .config/nvim/lua/plugins/git/core.lua
+++ .config/nvim/lua/plugins/git/core.lua
@@ -1,6 +1,16 @@
 return {
-  { "sindrets/diffview.nvim", cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" } },
-  { "tpope/vim-fugitive", cmd = "Git" },
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewFileHistory",
+    },
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = "Git",
+  },
   {
     "lewis6991/gitsigns.nvim",
     event = "BufReadPre",
