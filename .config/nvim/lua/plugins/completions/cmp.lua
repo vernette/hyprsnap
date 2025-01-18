aa5d097f964eec5ed174f012a7b96fb51cb3f028
Nikita Skryabin <nekit2271@gmail.com>
2025-01-18 17:05:34 +0300

    fix(cmp.lua): change InsertEnter to BufReadPost for nvim-cmp plugin initialization to improve completion behavior

--- .config/nvim/lua/plugins/completions/cmp.lua
+++ .config/nvim/lua/plugins/completions/cmp.lua
@@ -10,7 +10,7 @@ return {
   },
   {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    event = "BufReadPost",
     dependencies = {
       { "hrsh7th/cmp-nvim-lsp" },
       { "hrsh7th/cmp-path" },
